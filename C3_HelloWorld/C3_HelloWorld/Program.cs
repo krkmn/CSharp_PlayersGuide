b4d0ba82f90b4f8de611007e3085d2ad648f68f5
krkmn <owais.sulehria@gmail.com>
2018-12-15 11:18:40 +0100

    Chapter 3 examples

---
+++ C3_HelloWorld/C3_HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); // Prints out Hello, World!
+            Console.ReadKey(); /* Waits for key input */
+            Console.WriteLine("I'm sorry, Dave. I'm afraid I can't do that."); // Prints out a famous nerd quote!
+            Console.ReadKey(); /* Waits for key input */
+        }
+
+    }
+}
