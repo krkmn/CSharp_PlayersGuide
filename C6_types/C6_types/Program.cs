b8b7ba4eae9cb1a7b45c3c531d7d39154be25bb7
krkmn <owais.sulehria@gmail.com>
2018-12-15 15:57:10 +0100

    Chapter 6 example

---
+++ C6_types/C6_types/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C6_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first_string = "Hello, there";
+            Console.WriteLine(first_string);
+
+            bool first_bool = false;
+            Console.WriteLine(first_bool);
+
+            decimal first_decimal = 3.141592653589793M;
+            Console.WriteLine(first_decimal);
+
+            float first_float = 1.010f;
+            Console.WriteLine(first_float);
+
+            double first_double = 1.11111110;
+            Console.WriteLine(first_double);
+
+            byte first_byte = 200;
+            Console.WriteLine(first_byte);
+
+            sbyte first_sbyte = -100;
+            Console.WriteLine(first_sbyte);
+
+            int first_int = 1000000;
+            Console.WriteLine(first_int);
+
+            short first_short = 10000;
+            Console.WriteLine(first_short);
+
+            long first_long = 100000000;
+            Console.WriteLine(first_long);
+
+            uint first_uint = 1;
+            Console.WriteLine(first_uint);
+
+            ushort first_ushort = 10000;
+            Console.WriteLine(first_ushort);
+
+            ulong first_ulong = 123456112345;
+            Console.WriteLine(first_ulong);
+
+            char first_char = 'C';
+            Console.WriteLine(first_char);
+
+        }
+    }
+}
