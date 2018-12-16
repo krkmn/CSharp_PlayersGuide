ad4f3e4b70144a5c3027e6ed3271c163cd9830ad
krkmn <owais.sulehria@gmail.com>
2018-12-16 16:13:30 +0100

    Rest of chapter 13

---
+++ C13_ForeachMinAverage/C13_ForeachMinAverage/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C13_ForeachMinAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 45, 4, 124, 4, 22, 35, 3 };
+            int current_minimum = Int32.MaxValue;
+            int total = 0;
+
+            foreach (int item in array)
+            {
+                if (current_minimum > item)
+                    current_minimum = item;
+
+                total += item;
+            }
+
+            float average = (float)total / array.Length;
+
+            Console.WriteLine($"The minimum value of the array is {current_minimum} and the average is {average}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C13_MultiDimensionalArray/C13_MultiDimensionalArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C13_MultiDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] multidim_array = new int[4, 5];
+            multidim_array[1, 1] = 1;
+            int[][] matrix = new int[4][];
+            matrix[0] = new int[20];
+            matrix[1] = new int[20];
+            matrix[2] = new int[20];
+            matrix[3] = new int[20];
+            Console.WriteLine(multidim_array[1, 1]);
+            Console.WriteLine(multidim_array.Length); //Gives total elements in multidim array
+            Console.WriteLine(multidim_array.GetLength(0)); //Gives row length in multidim array
+            Console.WriteLine(multidim_array.GetLength(1)); //Gives column length in multidim array
+            Console.WriteLine(matrix.Length); //Gives row length in array of arrays
+
+            int i = 0;
+            foreach (int item in multidim_array) //Writing out every element in multidim_array
+            {
+                Console.WriteLine($"Item {i} is {item}");
+                ++i;
+            }
+        }
+    }
+}
