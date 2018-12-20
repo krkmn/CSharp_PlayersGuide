de15ffcaa6da5022a769d94f429dda093f933adc
krkmn <owais.sulehria@gmail.com>
2018-12-20 21:26:15 +0100

    Chapter 15, arrays and recursive functions

---
+++ C15_Fibonacci/C15_Fibonacci/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C15_Fibonacci
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Recursive function to find the Nth number in the Fibonacci Sequence
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns>Returns the Nth number of the Fibonacci Sequence</returns>
+        static int Fibonacci(int N)
+        {
+            if (N < 3)
+                return 1;
+            else
+                //Console.WriteLine(N);
+                return Fibonacci(N-1) + Fibonacci(N-2);
+        }
+
+        static void Main(string[] args)
+        {
+            int fib = 0;
+            int fib_old = 1;
+            int dummy;
+            for (int i = 1; i < 11; i++)
+            {
+                dummy = fib;
+                fib = fib + fib_old;
+                fib_old = dummy;
+                Console.Write(fib + " ");
+            }
+
+            Console.WriteLine();
+            for(int N = 1; N < 11; N++)
+                Console.WriteLine($"Number {N} of the Fibonacci Sequence is: {Fibonacci(N)}");
+            
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C15_ReverseArray/C15_ReverseArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C15_ReverseArray
+{
+    class Program
+    {
+       static int[] GenerateNumbers(int length)
+        {
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++)
+                array[i] = i;
+            return array;
+        }
+
+        static void Reverse(int[] args)
+        {
+            int dummy = 0;
+            for (int i = 0; i < args.Length / 2; ++i)
+            {
+                dummy = args[i];
+                args[i] = args[args.Length - i - 1];
+                args[args.Length - i - 1] = dummy;
+            }
+        }
+
+        static void PrintNumbers(int[] args)
+        {
+            foreach (int num in args)
+                Console.Write($"{num} ");
+        }
+
+        static void Main(string[] args)
+        {
+           int[] array = GenerateNumbers(12);
+           Reverse(array);
+           PrintNumbers(array);
+        }
+    }
+}
